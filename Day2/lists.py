5f336d3809684f524c3f0353ee460b8e0d36f3f0
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-26 12:41:11 +0530

    2nd problem

--- Day2/lists.py
+++ Day2/lists.py
@@ -1,5 +1,20 @@
-a=[5,10]
-for i in range(3):
-    a.append(int(input("Enter no:")))
+#problem 1
+# a=[5,10]
+# for i in range(3):
+#     a.append(int(input("Enter no:")))
     
+# print(a)
+
+
+a=[20,5,7,20,46,20,3,20]
+# using for loop
+# for i in x:
+#     if i==20:
+#         x.remove(20)
+
+#using while loop
+while 20 in a:
+    a.remove(20)
+        
 print(a)
+
