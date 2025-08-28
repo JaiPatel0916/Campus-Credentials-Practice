a031abcd72f2a0826d2d851a5f20e77fca34dbd5
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-28 15:35:45 +0530

    task 2 completed

--- Day4/task2.py
+++ Day4/task2.py
@@ -6,3 +6,24 @@
 
 # brand() → normal method that prints brand name
 # Create Fan and WashingMachine classes implementing turn_on().
+
+
+from abc import ABC, abstractmethod
+class Appliance(ABC):
+    @abstractmethod
+    def turn_on(self):
+        pass
+    def brand(self):
+        print("LG")
+class Fan(Appliance):
+    def turn_on(self):
+        print("Fan on")
+class WashingMachine(Appliance):
+    def turn_on(self):
+        print("Washing Machine is on")
+f=Fan()
+w=WashingMachine()
+f.turn_on()
+f.brand()
+w.turn_on()
+w.brand()
