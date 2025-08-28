8d55a369a0925da90f381fd05a00bddeaa4e957b
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-28 11:06:36 +0530

    Day 4 initial commit

--- Day3/self.py
+++ Day3/self.py
@@ -1,12 +1,37 @@
-class Ex:
-    def acceptData(self,a):
-        self.roll = a
+# class Ex:
+#     def acceptData(self,a):
+#         self.roll = a
         
-    def showData(self):
-        print(self.roll)
+#     def showData(self):
+#         print(self.roll)
 
-e1=Ex()
+# e1=Ex()
 
-e1.acceptData(5)
+# e1.acceptData(5)
 
-e1.showData()
+# e1.showData()
+
+
+class Ex:
+
+    def acp(self, x, y):
+        self.a = x
+        self.b = y
+
+    def cal(self, e1):
+        self.a = e1.a + self.a
+        self.b = e1.b + self.b
+
+    def dsp(self):
+        print(self.a, "\t", self.b)
+
+
+# main part
+e1 = Ex()
+e2 = Ex()
+
+e1.acp(3, 4)
+e2.acp(10, 20)
+
+e2.cal(e1)
+e2.dsp()
---
+++ Day4/self.py
@@ -0,0 +1,14 @@
+class Student:
+    def acceptData(self):
+        self.roll = int(input("Enter Data"))
+        
+        
+    
+    
+s1=Student()
+s1.acceptData()
+print(s1.roll)
+
+s2=Student()
+s2.acceptData()
+print(s2.roll)
