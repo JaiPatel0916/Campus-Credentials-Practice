dab96842f4d316a2b323faec6972ed9679b74db5
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-29 11:51:52 +0530

    Task 3 added

---
+++ Day4/task3.py
@@ -0,0 +1,11 @@
+# 3rd example 
+# Polymorphism with Abstract Class
+
+# Task: Create a payment system:
+
+# Abstract class Payment with pay(amount)
+
+# Classes: CreditCardPayment, UPIPayment, PayPalPayment
+# Loop through a list of payment objects and process payments.
+
+
