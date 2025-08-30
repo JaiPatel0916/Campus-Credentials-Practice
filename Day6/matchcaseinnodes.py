7010a8f5dbea7f48015cc6a95446ddc4a207b40c
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-30 12:54:16 +0530

    tried functions with matchcase

---
+++ Day6/matchcaseinnodes.py
@@ -0,0 +1,117 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next = None
+        
+class singlylinklist:
+    def __init__(self):
+        self.head=None
+        
+    def insertnodeatlast(self):
+        if self.head==None:
+            self.head = Node(int(input("Enter Data: ")))
+        else :
+            t=self.head
+            
+            while t.next!=None:
+                t=t.next
+            t.next =Node(int(input("Enter Data: ")))
+            
+            
+    def insertnodeatfirst(self):
+        temp = Node(int(input("Enter Data: ")))
+        temp.next = self.head
+        self.head= temp
+        
+    def displaynode(self):
+        t=self.head  
+        while t!=None:
+            print(t.data,end="-->")
+            t=t.next
+        
+    def countNode(self):
+        cnt=1
+        t=self.head
+        while t!=None:
+            cnt+=1
+            t=t.next
+        return(cnt)
+            
+    def insertNodeAnyWhere(self):
+        totalNodes=self.countNode()
+        position=int(input("Enter Position:"))
+        if position>=1 and position <= totalNodes+1:
+            if position==1:
+                self.insertNodeatfirst()
+            elif position==totalNodes+1:
+                self.insertnodeatlast()
+            else:   
+                t=self.head             
+                i=1
+                
+                while i<=position-2:
+                    t=t.next
+                tmp =Node(int(input("Enter Data")))
+                tmp.next = t.next
+                t.next=tmp
+        else:
+            print("Invalid Position")
+        totalNodes=self.countNode()
+        position=int(input("Enter Position:"))
+        if position>=1 and position<=totalNodes+1:
+            if position==1:
+                self.insertnodeatfirst()
+            elif position==totalNodes+1:
+                self.insertnodeatlast()
+            else:   
+                t=self.head             
+                for i in range(position-2):
+                    t=t.next
+                tmp=Node(int(input("Enter Position:")))
+                tmp.next=t.next
+                t.next=tmp
+        else:
+            print("Invalid Position")
+            
+            
+s=singlylinklist()
+
+while True:
+    print("\nMenu:")
+    print("1.Insert Node at First")
+    print("2. Insert node at last")
+    print("3. Insert Node at any where")
+    print("4. CountTotal nodes")
+    print("5. Display all the nodes")
+    print("0: Exit")
+    
+    choice = int(input("\nEnter your choice: "))
+    
+    match choice:
+        case 1 :
+            s.insertnodeatfirst()
+        case 2: 
+            s.insertnodeatlast()
+            
+        case 3:
+            s.insertNodeAnyWhere()
+        case 4:
+            print("\nTotal Nodes: ",s.countNode())
+
+            
+        case 5:
+            s.displaynode()
+            
+        case 0:
+            exit()
+            
+        case _:
+            print("\nMake a valid choice ")
+            
+    
+
+    
+
+
+
+         
