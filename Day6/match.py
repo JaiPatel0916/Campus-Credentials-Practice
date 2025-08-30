cea00001087f8a0dbb68da7242a5d9df876c6d6b
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-30 09:35:21 +0530

    type one done

---
+++ Day6/match.py
@@ -0,0 +1,10 @@
+choice = int(input("Entrt your chice: "))
+
+# match choice:
+#     case 1:
+#         print("ONE")
+#     case 2:
+#         print("TWO")
+#     case 3:
+#         print("Three")
+        
