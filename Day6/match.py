2e4799b264d50f5af97623efde8004b8c9631e98
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-30 09:56:46 +0530

    Task 3 done

--- Day6/match.py
+++ Day6/match.py
@@ -1,5 +1,6 @@
-choice = int(input("Entrt your chice: "))
+# choice = int(input("Entrt your chice: "))
 
+# TYPE 1
 # match choice:
 #     case 1:
 #         print("ONE")
@@ -8,9 +9,56 @@ choice = int(input("Entrt your chice: "))
 #     case 3:
 #         print("Three")
         
+# TYPE 2
+# match choice:
+#     case 1|2|3:
+#         print("Hello")
+#     case 4|5|6:
+#         print("JAI")
+
+
+#TYPE 3
+# x=["Jai","Rashi","Shivam"]
+
+# match x:
+#     case 8.2:
+#         print("Float")
+#     case ["Jai","Shivam"]:
+#         print("Boys")
+#     case ["Rashi"]:
+#         print("Girl")
+#     case [1,2,3]:
+#         print("Numbers")
+#     case x:
+#         print("Printed x")
+
+
+#TASK 2 
+    
+# t=("Car","Bike")
+
+# match t:
+#     case ("Car","Bike"):
+#         print("Vehicles")
+#     case ("8","50"):
+#         print("Numbers")
+
+
+#Task 3
+
+choice = int(input("Enter your choice: "))
 
 match choice:
-    case 1|2|3:
-        print("Hello")
-    case 4|5|6:
-        print("JAI")
+    case 1:
+       a= int(input("Enter first number: "))
+       b= int(input("Enter first number: "))
+       print(a+b)
+    case 2:
+        c= int(input("Enter your number: ")) 
+        print(c*c)
+    case 3:
+        r= int(input("Enter the radius: ")) 
+        print(3.14*r*r)
+        
+
+
