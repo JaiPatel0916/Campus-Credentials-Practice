784d081738cbfc717753b06225be070ba511995e
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-31 11:44:46 +0530

    circular link list implemented

--- Day7/circularlinklist.py
+++ Day7/circularlinklist.py
@@ -1,53 +1,80 @@
 class Node:
     def __init__(self,data):
-        self.data= data
+        self.data=data
         self.next=None
-        
-class circularlinklist:
+
+class CircularLinkedList:
     def __init__(self):
         self.head=None
-    def InserNodeAtLast(self):
+    
+    def insertNodeAtLast(self):
         if self.head==None:
-            self.head=Node(int(input("Enter Data: ")))
+            self.head=Node(int(input("Enter data:" )))
             self.head.next=self.head
         else:
             t=self.head
             while t.next!=self.head:
                 t=t.next
-            t.next=Node(int(input("Enter Data: ")))
+            t.next=Node(int(input("Enter data:")))
             t.next.next=self.head
-    
-    
-    def InsertNodeAtFirst(self):
+
+    def insertNodeAtFirst(self):
         if self.head==None:
-            self.InserNodeAtLast()
+            self.head=Node(int(input("Enter data:")))
+            self.head.next=self.head
         else:
             t=self.head
             while t.next!=self.head:
                 t=t.next
-            tmp=Node(int(input("Enter data: ")))
+            tmp=Node(int(input("Enter data:")))
             tmp.next=self.head
             self.head=tmp
             t.next=self.head
-                
-                
-            
+    
+    def deleteFirstNode(self):
+        if self.head==None:
+            print("List is empty")
+        else:
+            if self.head!=None:
+                t=self.head
+                while t.next!=self.head:
+                    t=t.next
+                t.next=self.head.next
+                self.head.next=None
+                self.head=t.next
+                t=None
    
+    def deleteLastNode(self):
+        if self.head==None:
+            print("List is empty")
+        else:
+            if self.head.next==self.head:
+                self.head=None
+            else:
+                t=self.head
+                while t.next.next!=self.head:
+                    t=t.next
+                tmp=t.next
+                t.next=self.head
+                tmp.next=None
+                tmp=None
+
     def display(self):
         if self.head==None:
-            print("Link list is empty")
+            print("List is empty")
         else:
             t=self.head
-            
             while True:
-                print(t.data,end="-->")
+                print(t.data,end="->")
                 t=t.next
                 if t==self.head:
                     break
-                
-l=circularlinklist()
-l.InserNodeAtLast()
-l.InserNodeAtLast()
-l.InserNodeAtLast()
-l.InsertNodeAtFirst()
+    
+l=CircularLinkedList()
+l.insertNodeAtLast()
+l.insertNodeAtLast()
+l.insertNodeAtLast()
+l.insertNodeAtFirst()
+l.deleteFirstNode()
+l.deleteLastNode()
 l.display()
